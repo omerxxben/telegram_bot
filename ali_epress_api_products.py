fd63b8603429fc716ff2dbeddc78e8b7e7e1e4c8
Benfad <benfad.developer@gmail.com>
2025-06-21 16:27:59 +0300

    added console print of pd df

--- ali_epress_api_products.py
+++ ali_epress_api_products.py
@@ -5,30 +5,16 @@ import time
 
 class AliExpressApiProducts:
     def process(self, products):
-        timestamp = int(time.time() * 1000)
-        params = {
-            "app_key": APP_KEY,
-            "method": "aliexpress.affiliate.product.sku.detail.get",
-            "sign_method": "md5",
-            "format": "json",
-            "v": "2.0",
-            "timestamp": timestamp,
-            "ship_to_country": "IL",
-            "product_id": 1005007859139314,
-            "target_currency": "ILS",
-            "target_language": "HE",
-        }
-        params["sign"] = self.generate_signature(params)
-        try:
-            response = requests.get(URL, params=params)
-            response.raise_for_status()
-            return response.json()
-        except Exception as e:
-            return {"error": str(e)}
-
-    def generate_signature(self, params: dict) -> str:
-        sorted_params = ''.join(f'{k}{v}' for k, v in sorted(params.items()))
-        sign_str = f"{APP_SECRET}{sorted_params}{APP_SECRET}"
-        return hashlib.md5(sign_str.encode('utf-8')).hexdigest().upper()
-
-
+        appkey = "516068"
+        appSecret = "eCMr5mVeMOEOC3mTgagoGa0e3Fw0fkNZ"
+        client = iop.IopClient(URL, appkey, appSecret)
+        request = iop.IopRequest('aliexpress.ds.product.get')
+        request.add_api_param('ship_to_country', 'US')
+        request.add_api_param('product_id', '1005003784285827')
+        request.add_api_param('target_currency', 'USD')
+        request.add_api_param('target_language', 'en')
+        request.add_api_param('remove_personal_benefit', 'false')
+        request.add_api_param('biz_model', 'biz_model')
+        response = client.execute(request, access_token)
+        print(response.type)
+        print(response.body)
--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from ali_epress_api import AliExpressApi
 from products_transform import ProductsTransform
 
 if __name__ == "__main__":
-    product_name = "iphone 16 screen protector"
+    product_name = ("BOLBOL")
     products = AliExpressApi().process(product_name, 1)
     print(json.dumps(products, indent=4, ensure_ascii=False))
-    products = AliExpressApiProducts().process(products)
+
+    #products = AliExpressApiProducts().process(products)
+    print(json.dumps(products, indent=4, ensure_ascii=False))
+
     table = ProductsTransform().transform_to_table(products)
     pretty_print_df(table)
