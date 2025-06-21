b519ac65f39cabc4dd257b892bbe3a608f413702
Benfad <benfad.developer@gmail.com>
2025-06-21 18:52:15 +0300

    finished the image genrator

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from products_transform import ProductsTransform
 
 if __name__ == "__main__":
     #print(json.dumps(products, indent=4, ensure_ascii=False))
-    product_name = ("tennis ball")
+    product_name = ("jbl flip 6 speaker")
     products = AliExpressApi().process(product_name, 10)
     products_df = ProductsTransform().transform_to_table(products)
     products_df_detailed = AliExpressApiProducts().process(products_df)
