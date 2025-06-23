c64e574796601bf07d141913bc6ab2e759d3e105
Your Name <omeryomtov@gmail.com>
2025-06-23 21:02:57 +0300

    translate

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from products_transform import ProductsTransform
 
 if __name__ == "__main__":
     #print(json.dumps(products, indent=4, ensure_ascii=False))
-    product_name = "jbl flip 6 speaker"
+    product_name_hebrew = "גבל פליפ 6"
+    #product_name_english = Translate.process(product_name_hebrew)
+    product_name_english = "jbl flip 6 speaker"
     products = AliExpressApi().process(product_name, 100)
     products_df = ProductsTransform().transform_to_table(products)
     products_df_detailed = AliExpressApiProducts().process(products_df)
-
     products_df_rank = getRank().calculate(products_df_detailed)
     #products_df_relevant = CheckRelevant().check(products_df_rank)
     creator = ImageGridCreator(grid_size=(800, 800))
