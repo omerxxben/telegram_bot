6c51db672198f4c7ec70db7aef1df8fbef8f6e9b
Your Name <omeryomtov@gmail.com>
2025-06-26 18:38:43 +0300

    added images

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     OUTPUT_PATH = r"C:\Users\User\OneDrive\שולחן העבודה\images\grid.jpg"
     #OUTPUT_PATH = r"C:\Users\User\Desktop\images\grid.jpg"
     ai_manager = AIManager()
-    search_query = "חפש לי jbl flip 6"
+    search_query = "חפש לי שלט לפלייסטישן 5"
     product_name_english = ai_manager.translate_hebrew_query(search_query)
-    products = AliExpressApi().process(product_name_english,  2)
+    products = AliExpressApi().process(product_name_english,  49)
     products_df = ProductsTransform().transform_to_table(products)
     if len(products) != 0:
         products_df_rank = getRank().sort_by_volume(products_df)
-        #products_df_filtered_by_title = ai_manager.get_suitable_titles(product_name_english, products_df_rank)
+        products_df_filtered_by_title = ai_manager.get_suitable_titles(product_name_english, products_df_rank)
         products_df_detailed = AliExpressApiProducts().process(products_df_rank)
         pretty_print_df(products_df_detailed)
         creator = ImageGridCreator(grid_size=(800, 800))
