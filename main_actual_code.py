2a360c9c547726d1be1f061c2732e2e6e0c8bb8e
Your Name <omeryomtov@gmail.com>
2025-06-27 17:19:25 +0300

    omer changes

--- main_actual_code.py
+++ main_actual_code.py
@@ -15,30 +15,23 @@ class MainProducts:
         self.ai_manager = AIManager()
         start_time = time.time()
         timings = {}
-
         product_name_english = self.ai_manager.translate_hebrew_query(search_query)
         if not product_name_english:
             print("No product name")
             return []
-
         products, timings['get_products'] = AliExpressApi().process(product_name_english, 50)
         products_df, timings['transform_table'] = ProductsTransform().transform_to_table(products)
-
         if products_df.empty:
             print("Didn't find products")
             return []
-
         ranked_df = getRank().sort_by_volume(products_df)
         filtered_df = self.ai_manager.get_suitable_titles(product_name_english, ranked_df)
-
         if filtered_df.empty:
             print("Didn't find matching products via AI")
             return []
-
         products_df_detailed, timings['get_details'] = AliExpressApiProducts().process(filtered_df)
         products_df_detailed, timings['get_short_link'] = AliExpressApiShortLink().process(products_df_detailed)
         image_bytes_io, timings['get_image'] = self.creator.save_grid(products_df_detailed, IS_PRINT_IMAGE)
-
         selected_columns = [
             "target_sale_price",
             "avg_evaluation_rating",
@@ -49,16 +42,13 @@ class MainProducts:
         ]
         filtered_df = products_df_detailed[selected_columns].copy()
         products_list = filtered_df.to_dict(orient="records")
-
         if IS_LOGS:
             timings['total'] = time.time() - start_time
             Logger().ali_express_log( search_query, product_name_english, products_df_detailed, self.ai_manager, timings,
             )
-
         transformed_products = [
             ProductsTransform().transform_product_names(product) for product in products_list
         ]
-
         return {
             "image_bytes_io": image_bytes_io,
             "products_list": transformed_products,
