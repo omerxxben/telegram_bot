3b6efb2470937205b6d0556fbeefd0c74f7eb616
Your Name <benfad33@gmail.com>
2025-06-29 13:33:34 +0300

    fake web

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
 import threading
-
 from flask import Flask
-
 from Telegram.telegram_bot_manager import TelegramBotManager
+
 app = Flask(__name__)
 
-
-def main() -> None:
+def start_bot():
     bot_token = "7873230963:AAH0Z1rAPqLlqvXMzpYasR8NMyk4qPn-ANY"
     bot = TelegramBotManager(bot_token)
     bot.run()
 
+@app.route('/')
+def home():
+    return 'Bot is running!'
 
 if __name__ == '__main__':
-    main()
+    # Start the bot in a background thread
+    threading.Thread(target=start_bot).start()
 
-if __name__ == '__main__':
-    threading.Thread(target=main).start()
-    port = int(os.environ.get("PORT", 10000))  # Render provides $PORT
+    # Bind to the port Render gives us
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
